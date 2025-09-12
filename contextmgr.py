2325e592c68182313bd6653ad797b52d8c7f5991
bsg-saifx <asfaq.mahmud@blockstak.ai>
2025-09-12 15:50:57 +0600

    Custom Context mgr

---
+++ contextmgr.py
@@ -0,0 +1,14 @@
+import time
+
+class Timer:
+    def __enter__(self):
+        self.start = time.perf_counter() 
+        return self
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.end = time.perf_counter()  
+        self.interval = self.end - self.start
+        print(f"Elapsed time: {self.interval:.6f} seconds")
+
+with Timer() as t:
+    for _ in range(1000000):
+        pass
